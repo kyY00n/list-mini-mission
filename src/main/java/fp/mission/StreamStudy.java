3a707bb53da8bac7b449456f3e6e0efd3c811f5f
Rosie <dev.kayoung@gmail.com>
2023-04-11 02:56:54 +0900

    refactor: for문을 stream API로 리팩터링

--- src/main/java/fp/mission/StreamStudy.java
+++ src/main/java/fp/mission/StreamStudy.java
@@ -3,9 +3,9 @@ package fp.mission;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamStudy {
 
@@ -14,32 +14,17 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt"));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) {
-                count++;
-            }
-        }
-        return count;
+        return words.stream()
+                .filter(word -> word.length() > 12)
+                .count();
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        List<Integer> result = new ArrayList<>();
-        for (Integer number : numbers) {
-            result.add(2 * number);
-        }
-
-        return result;
+        return numbers.stream().map(number -> 2 * number).collect(Collectors.toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
-        int result = 0;
-
-        for (Integer number : numbers) {
-            result += number;
-        }
-
-        return result;
+        return numbers.stream().mapToInt(number -> number).sum();
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
